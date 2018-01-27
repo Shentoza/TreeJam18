5186d34b177e6b26c019fca830f8c852e3f095b6
Tobias Rogalski <dante.rogalski@gmail.com>
2018-01-27 02:33:49 +0100

    merge

--- Assets/Scripts/Mushroom/Mushroom.cs
+++ Assets/Scripts/Mushroom/Mushroom.cs
@@ -7,9 +7,9 @@ public class Mushroom : MonoBehaviour
     public float range;
     int sporeCost;
     static int count;
-    MushroomManager manager;
+    public MushroomManager manager;
     List<Mushroom> mushroomNeighbors;
-    List<ShroomTree> treeNeighbors;
+    public List<ShroomTree> treeNeighbors;
 
 	// Use this for initialization
 	void Start () {
@@ -21,21 +21,12 @@ public class Mushroom : MonoBehaviour
 		
 	}
 
-    void detectNeighbors()
-    {
-        foreach(Mushroom m in manager.mushrooms)
-        {
-            if (Vector3.Distance)
-            mushroomNeighbors.Add(m);
-        }
-    }
-
-    void deleteTree(ShroomTree s)
+    public void deleteTree(ShroomTree s)
     {
         treeNeighbors.Remove(s);
     }
 
-    void OnTriggerEnter(Collision c)
+    void OnTriggerEnter(Collider c)
     {
         if (c.gameObject.GetComponent<ShroomTree>())
         {
@@ -47,11 +38,23 @@ public class Mushroom : MonoBehaviour
         }
     }
 
+<<<<<<< HEAD
     public int Cost
     {
         get
         {
             return sporeCost;
+=======
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.GetComponent<ShroomTree>())
+        {
+            treeNeighbors.Remove(c.gameObject.GetComponent<ShroomTree>());
+        }
+        else if (c.gameObject.GetComponent<Mushroom>())
+        {
+            mushroomNeighbors.Remove(c.gameObject.GetComponent<Mushroom>());
+>>>>>>> dca79814ca36d03b38cbc322290eb0873b7e5c87
         }
     }
 }

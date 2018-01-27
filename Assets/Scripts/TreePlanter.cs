d2577ed8457433cccc2cc7769b58a4ebbaeb1359
Skeletti <schreiber.daniel87@gmail.com>
2018-01-28 00:25:32 +0100

    Destroy BuildCollider in TreePlanter.cs

--- Assets/Scripts/TreePlanter.cs
+++ Assets/Scripts/TreePlanter.cs
@@ -23,17 +23,16 @@ public class TreePlanter : MonoBehaviour {
         bool freeSpace = false;
         while (!freeSpace)
         {
+            //Find x,y on plane
             float x = Random.Range(transform.localScale.x * (-5.0f), transform.localScale.x * 5.0f);
             float z = Random.Range(transform.localScale.z * (-5.0f), transform.localScale.z * 5.0f);
 
             Ray ray = new Ray(new Vector3(x, 100, z), new Vector3(0, -1, 0));
-            //Debug.DrawRay(new Vector3(x, 100, z), new Vector3(0, -1, 0));
             RaycastHit hit;
-
             Physics.Raycast(ray, out hit);
 
-           if (hit.collider.GetComponentInParent<ShroomTree>() != null)
-           {
+            if (hit.collider.GetComponentInParent<ShroomTree>() != null)
+            {
                 continue;
             }              
             else
@@ -48,20 +47,20 @@ public class TreePlanter : MonoBehaviour {
 
     public void plantTrees()
     {
-        List<ShroomTree> allTrees = new List<ShroomTree>();
+        List<ShroomTree> listOfTrees = new List<ShroomTree>();
         int planted = 0;
+
         while (planted < numOfTrees)
         {
             Vector3 position;
             position = getTreePosition();
             float treeModel = Random.Range(0, 100);
-            allTrees.Add(Instantiate<ShroomTree>((treeModel <= 50.0f) ? tree1 : tree2, position, tree1.transform.rotation));
+            listOfTrees.Add(Instantiate<ShroomTree>((treeModel <= 50.0f) ? tree1 : tree2, position, tree1.transform.rotation));
             planted++;
         }
 
-        foreach(ShroomTree tree in allTrees)
+        foreach(ShroomTree tree in listOfTrees)
         {
-            Debug.Log("Hallo");
             Destroy(tree.transform.GetChild(2).gameObject);
         }
     }
